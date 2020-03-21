d29f482b617236d73c83a313834bf6b10c779e50
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-21 03:04:25 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,4 +1,4 @@
-class AddCharacterCatchphrase < ActiveRecord::Migration[5.1]
+class AddCatchPhraseToCharacters < ActiveRecord::Migration[5.1]
   def change
     add_column :characters, :catchphrase, :string
   end
